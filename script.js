ea8eafeeaeec9298cf65d26b506b6dea7ed01106
Bhagya <janabhagyalakshmi00@gmail.com>
2026-05-25 13:20:44 +0530

    Initial commit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const apiKey = "0e9e8e4c22740465c7853f0d79dd7e29";
 
 async function getWeather(){
@@ -40,3 +41,47 @@ else if(data.weather[0].main=="clear"){
     `Wind Speed: ${data.wind.speed} m/s`;
 
 }
+=======
+const apiKey = "5401a1774f8da764de6ab8d0db275123";
+
+async function getWeather(){
+
+  const city = document.getElementById("city").value;
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  const response = await fetch(url);
+
+  const data = await response.json();
+let icon="☀";
+if(data.weather[0].main=="clouds"){
+  icon="☁";
+}
+else if(data.weather[0].main=="rain"){
+  icon="🌧";
+}
+else if(data.weather[0].main=="clear"){
+  icon="☀";
+}
+  console.log(data);
+  if(data.cod!=200){
+    alert(data.message);
+    return;
+  }
+  document.getElementById("cityName").innerHTML =
+    `${icon} ${data.name}`;
+
+  document.getElementById("temperature").innerHTML =
+    `Temperature: ${data.main.temp} °C`;
+
+  document.getElementById("description").innerHTML =
+    `Weather: ${data.weather[0].description}`;
+
+  document.getElementById("humidity").innerHTML =
+    `Humidity: ${data.main.humidity}%`;
+
+  document.getElementById("wind").innerHTML =
+    `Wind Speed: ${data.wind.speed} m/s`;
+
+}
+>>>>>>> 1063fd2 (updated weather app)
